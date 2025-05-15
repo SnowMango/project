3ef2cf2cb1ee5a7732a1243f14d807fb158dc91c
FengZheng <164804868@qq.com>
2025-05-15 15:25:51 +0800

    浮窗 回弹和阻尼效果

--- ZQSubject/Sources/Modules/Home/View/LevitateView.swift
+++ ZQSubject/Sources/Modules/Home/View/LevitateView.swift
@@ -11,7 +11,6 @@ class LevitateView: UIView {
         setupUI()
     }
     private var start: CGAffineTransform = .identity
-    private var startFrame: CGRect = .zero
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -24,7 +23,7 @@ class LevitateView: UIView {
         }
         JumpManager.jumpToWeb(item.linkAddress)
     }
-    
+
     @objc func panAction(_ pan: UIPanGestureRecognizer) {
         guard let pv = pan.view else { return }
         guard let superview = superview else { return }
@@ -35,27 +34,56 @@ class LevitateView: UIView {
         }
         let translate = pan.translation(in: pv)
         let willTo = self.start.translatedBy(x: translate.x, y: translate.y)
+       
+        let willFrame = pv.frame.applying(pv.transform.inverted()).applying(willTo)
         if pan.state != .changed {
-            let offset: CGPoint = self.offset(pv.frame, in: enbaleFrame)
+            let bounces = self.bounces(willFrame, in: enbaleFrame)
             UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: [.curveEaseOut], animations: {
-                pv.transform = willTo.translatedBy(x: offset.x, y: offset.y)
+                pv.transform = willTo.translatedBy(x: bounces.x, y: bounces.y)
             })
             return
         }
-        pv.transform = willTo
+        let damping = self.damping(willFrame, in: enbaleFrame)
+        pv.transform = willTo.translatedBy(x: damping.x, y: damping.y)
     }
-    private func offset(_ frame: CGRect, in enable:CGRect) -> CGPoint {
+
+    private func bounces(_ frame: CGRect, in enable:CGRect) -> CGPoint {
         var offset: CGPoint = .zero
         if frame.midX < enable.midX {
             offset.x = enable.minX - frame.minX
         }else {
             offset.x = enable.maxX - frame.maxX
         }
-
+        
         if frame.minY < enable.minY {
             offset.y = enable.minY - frame.minY
         } else  if frame.maxY > enable.maxY {
             offset.y = enable.maxY -  frame.maxY
+        }
+        return offset
+    }
+    
+    private func damping(_ frame: CGRect, in enable:CGRect) -> CGPoint {
+        let m: CGPoint = CGPoint(x: frame.width, y: frame.height)
+        if m.x == 0 || m.y == 0 { return .zero }
+        var distance: CGPoint = .zero
+        if frame.minX < enable.minX {
+            distance.x = enable.minX - frame.minX
+        }else if frame.maxX > enable.maxX {
+            distance.x = enable.maxX - frame.maxX
+        }
+        if frame.minY < enable.minY {
+            distance.y = enable.minY - frame.minY
+        }else if frame.maxY > enable.maxY {
+            distance.y = enable.maxX - frame.maxX
+        }
+    
+        var offset: CGPoint = .zero
+        if distance.x != 0 {
+            offset.x = distance.x - log(1 + abs(distance.x)/m.x) * m.x * distance.x / abs(distance.x)
+        }
+        if distance.y != 0 {
+            offset.y = distance.y - log(1 + abs(distance.y)/m.y) * m.y * distance.y / abs(distance.y)
         }
         return offset
     }
